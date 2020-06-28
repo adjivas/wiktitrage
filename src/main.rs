b034ef138b573ccec85044de99ae8a6f79c6bdca
adjivas <adjivas@student.42.fr>
2020-06-28 21:38:17 +0200

    gtk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use gio::prelude::*;
+use gtk::prelude::*;
+use gtk::ApplicationWindow;
+
+use std::env::args;
+
+use gdk::Screen;
+
 use url::Url;
 use serde_derive::Deserialize;
 
@@ -32,5 +40,102 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json::<Wiki>()
         .await?;
     println!("{:?}", resp.query.pages.iter().next());
+
+    let application = gtk::Application::new(
+        Some("org.main_window"),
+        gio::ApplicationFlags::empty()
+    )
+    .expect("Initialization failed...");
+
+    let height = Screen::height();
+    let style = format!("#label1 {{
+                            color: white;
+                            font-size: {0}px;
+                            text-shadow:  -{1}px -{1}px 0 black,
+                                           {1}px -{1}px 0 black,
+                                          -{1}px  {1}px 0 black,
+                                           {1}px  {1}px 0 black;
+                        }}", height/15, height/500);
+    application.connect_startup(move |app| {
+        // The CSS "magic" happens here.
+        let provider = gtk::CssProvider::new();
+        provider
+            .load_from_data(style.as_bytes())
+            .expect("Failed to load CSS");
+        // We give the CssProvided to the default screen so the CSS rules we added
+        // can be applied to our window.
+        gtk::StyleContext::add_provider_for_screen(
+            &gdk::Screen::get_default()
+                         .expect("Error initializing gtk css provider."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+
+        // We build the application UI.
+        build_ui(app);
+    });
+
+    let clipboard = gtk::Clipboard::get(&gdk::SELECTION_CLIPBOARD);
+    clipboard.request_text(|_, t| println!("{:?}", t));
+
+    application.run(&args().collect::<Vec<_>>());
+
     Ok(())
+}
+
+fn build_ui(application: &gtk::Application) {
+    let window = ApplicationWindow::new(application);
+    set_visual(&window, None);
+
+    window.connect_screen_changed(set_visual);
+    window.connect_draw(draw);
+
+    window.set_title("Wiktitrage");
+    window.set_app_paintable(true); // crucial for transparency
+
+    // The container container.
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+
+    let label = gtk::Label::new(Some("Some text"));
+    gtk::WidgetExt::set_widget_name(&label, "label1");
+
+    vbox.add(&label);
+    // Then we add the container inside our window.
+    window.add(&vbox);
+
+    let w = window.clone();
+    application.connect_activate(move |_| {
+        w.show_all();
+    });
+
+    let mut counter: usize = 5;
+    // we are using a closure to capture the label (else we could also use a normal function)
+    let tick = move || {
+        if let  Some(r) = counter.checked_sub(1) {
+            counter = r;
+            glib::Continue(true)
+        } else {
+            window.close();
+            glib::Continue(false)
+        }
+    };
+
+    // executes the closure once every second
+//    gtk::timeout_add_seconds(1, tick);
+}
+
+fn set_visual(window: &ApplicationWindow, _screen: Option<&gdk::Screen>) {
+    if let Some(screen) = window.get_screen() {
+        if let Some(ref visual) = screen.get_rgba_visual() {
+            window.set_visual(Some(visual)); // crucial for transparency
+        }
+    }
+}
+
+fn draw(_window: &ApplicationWindow, ctx: &cairo::Context) -> Inhibit {
+    // crucial for transparency
+    ctx.set_source_rgba(0.0, 0.0, 0.0, 0.0);
+    ctx.set_operator(cairo::Operator::Screen);
+    ctx.paint();
+    Inhibit(false)
 }
